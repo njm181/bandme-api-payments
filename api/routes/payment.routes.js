99468de83fb98459048aef5179ec0bf3108243f0
Nicolas Molina <nicolasjmolina1@gmail.com>
2022-10-01 16:51:18 -0300

    update

--- api/routes/payment.routes.js
+++ api/routes/payment.routes.js
@@ -1,13 +1,16 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { createCheckoutController } = require('../controller/payment.controller');
+const logger = require('heroku-logger')
+
 
 const router = Router();
 
 
 router.post('/checkout', createCheckoutController);
 
-router.post('/prueba', function(req, res) {
+router.get('/prueba', function(req, res) {
+    logger.info('LOG PROPIOOOOOOOOOooooooooooooooooooooooo')
     res.send('Ruta de prueba: ');
 })
 
