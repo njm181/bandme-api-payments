f77df5a38b612346c40dfbac37d4834ad7ff95f7
Nicolas Molina <nicolasjmolina1@gmail.com>
2022-10-05 10:15:22 -0300

    pruebas

--- api/routes/payment.routes.js
+++ api/routes/payment.routes.js
@@ -6,6 +6,11 @@ const logger = require('heroku-logger')
 
 const router = Router();
 
+router.get('/', function(req, res){
+    console.log(new Date().toLocaleString())
+    res.send('date')
+})
+
 router.post('/checkout', createCheckoutController);
 
 router.post('/checkout-payment-data', checkoutPaymentDataController)
